33534a7bbb6eb5f9f7785b77e4dab9b203371d22
padmakarsrinivas <srinivas_padmakar@yahoo.com>
2019-01-31 17:02:01 +0530

    Checking Page Factory

--- src/test/java/AppiumAWSTest/AppiumSample/AndoridNativeAWSTest.java
+++ src/test/java/AppiumAWSTest/AppiumSample/AndoridNativeAWSTest.java
@@ -8,10 +8,24 @@ import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.iOSFindBy;
 
 public class AndoridNativeAWSTest {
   public static AndroidDriver<?> mobiledriver;
+
+  @AndroidFindBy(id = "com.android.calculator2:id/digit_2")
+  @iOSFindBy(id ="2")
+  public MobileElement digit2;
+
+  @AndroidFindBy(id = "com.android.calculator2:id/digit_8")
+  @iOSFindBy(id ="2")
+  public MobileElement digit8;
+
   @BeforeTest
   public void beforeTest( ) throws MalformedURLException {
     DesiredCapabilities capabilities = new DesiredCapabilities();
@@ -26,10 +40,17 @@ public class AndoridNativeAWSTest {
 
   @Test
   public void launchApp() {
-    mobiledriver.findElementByAccessibilityId("New note").click();
-    mobiledriver.findElementById("com.example.android.notepad:id/note").sendKeys("New Note from test");
-    mobiledriver.findElementByAccessibilityId("Save").click();
-    String noteName = mobiledriver.findElementById("android:id/text1").getText();
-    Assert.assertEquals(noteName, "New Note from test", "Name Mismatch");
+
+    digit2.click();
+    mobiledriver.findElementByAccessibilityId("add").click();
+    digit8.click();
+    mobiledriver.findElementByAccessibilityId("equals").click();
+    String results=mobiledriver.findElementById("Result").getText();
+    Assert.assertEquals(results, "10", "Result Mismatch");
+    //    mobiledriver.findElementByAccessibilityId("New note").click();
+    //    mobiledriver.findElementById("com.example.android.notepad:id/note").sendKeys("New Note from test");
+    //    mobiledriver.findElementByAccessibilityId("Save").click();
+    //    String noteName = mobiledriver.findElementById("android:id/text1").getText();
+    //    Assert.assertEquals(noteName, "New Note from test", "Name Mismatch");
   }
 }
